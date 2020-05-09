e9eba14b1fb192e8e54b89977deb298fd69062bd
Nisha <nishasinha68@gmail.com>
2020-05-09 19:02:00 +0530

    getting bad records in missing and extra and null scenarios.

--- src/main/scala/SchemaValidation.scala
+++ src/main/scala/SchemaValidation.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.sql
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{DataType, IntegerType, StringType, StructField, StructType}
 
 object SchemaValidation {
   val givenSchema =
@@ -17,29 +17,39 @@ object SchemaValidation {
     .appName("validate schema")
     .getOrCreate()
 
-  def validate(dataPath: String): (Boolean, Set[StructField], Set[StructField]) = {
-    val dataDF = spark.read.format("parquet").load(dataPath)
+  def validate(dataPath: String):
+  (Boolean, Array[(String, DataType)], Array[(String, DataType)],sql.DataFrame) = {
+    var result = true
+    val dataDF = spark.read.option("mergeSchema", "true").load(dataPath)
     val dataSchema = dataDF.schema
 
     val columnCountMatch = givenSchema.fields.length == dataSchema.fields.length
     val (missingCols, extraCols) = getMissingAndExtraColumnsFromData(dataSchema)
 
-    (columnCountMatch, missingCols, extraCols)
+    if(missingCols.length > 0){
+      result = false
+      return (columnCountMatch, missingCols, extraCols, dataDF)
+    }
+
+    if(extraCols.length > 0){
+      result = false
+      val extraColName = extraCols(0)._1
+      val extraFieldBadRecords = dataDF.filter(dataDF.col(extraColName).isNotNull).toDF()
+      return (columnCountMatch, missingCols, extraCols, extraFieldBadRecords)
+    }
+
+    // column count match here
+    val nullViolatingRecords = dataDF.filter(dataDF.col("emp_id").isNull).toDF()
+    result = nullViolatingRecords.count() == 0
+    (columnCountMatch, missingCols, extraCols, nullViolatingRecords)
   }
 
   private def getMissingAndExtraColumnsFromData(dataSchema: StructType) = {
-    val dataSchemaWithoutNulls = dataSchema.map(_.copy(nullable = true)).toSet
-    val givenSchemaWithoutNulls = givenSchema.map(_.copy(nullable = true)).toSet
+    val dataSchemaWithoutNulls = dataSchema.fields.map(x => (x.name, x.dataType))
+    val givenSchemaWithoutNulls = givenSchema.fields.map(x => (x.name, x.dataType))
+
     val missingCols = givenSchemaWithoutNulls.diff(dataSchemaWithoutNulls)
     val extraCols = dataSchemaWithoutNulls.diff(givenSchemaWithoutNulls)
     (missingCols, extraCols)
   }
-
-  def getBadRecords(dataPath:String)={
-    val dataDF = spark.read.format("parquet").load(dataPath)
-    val nullViolatingRecords = dataDF.filter(dataDF.col("emp_id").isNull)
-    nullViolatingRecords.toDF()
-  }
-
-
 }
--- src/test/scala/SchemaValidationTest.scala
+++ src/test/scala/SchemaValidationTest.scala
@@ -10,77 +10,189 @@ class SchemaValidationTest extends FunSpec{
     .getOrCreate()
   spark.sparkContext.setLogLevel("ERROR")
 
-  val dataPath = "src/it/resources/data"
-  private def writeData(df: DataFrame): Unit ={
+  val dataPath = "src/resources/data"
+  private def writeData(df: DataFrame, mode:String = "overwrite"): Unit ={
     df.write.format("parquet")
-      .mode("overwrite")
+      .mode(mode)
       .save(dataPath)
   }
 
-  describe("Schema Validation"){
-    it("should return true when number of columns, column emp_name and type match"){
+  private def schemaMatch(df1:DataFrame, df2:DataFrame): Boolean ={
+    val x = df1.schema.fields.map(x => (x.name, x.dataType))
+    val y = df2.schema.fields.map(x => (x.name, x.dataType))
+    x.diff(y).isEmpty && y.diff(x).isEmpty
+  }
+
+  private def dataMatch(df1:DataFrame, df2:DataFrame): Boolean ={
+    // TODO: this comparison is still dependent on order of columns within a single row in database,
+    //  see how can that be mitigated...
+    (df1.count() == df2.count())
+      (df1.except(df2).isEmpty) &&
+      (df2.except(df1).isEmpty)
+  }
+
+  describe("Schema Validation, when all records have one schema"){
+
+    it("should return no bad records when number of columns, column name and type match"){
       import spark.implicits._
       val df = Seq((1, "Adam"), (2, "Eve")).toDF("emp_id", "emp_name")
       writeData(df)
-      assert(SchemaValidation.validate(dataPath) == (true, Set(), Set()))
+
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
+
+      assert(colCountMatch)
+      assert(missingFields.isEmpty)
+      assert(extraFields.isEmpty)
+      assert(actualBadRecords.collect().isEmpty)
     }
 
-    it("should return false and missing columns when missing columns"){
+    it("should return the missing column and all records when missing column in all records"){
       import spark.implicits._
       val df = Seq((1), (2)).toDF("emp_id")
       writeData(df)
-      assert(SchemaValidation.validate(dataPath) ==
-        (false, Set(StructField("emp_name",StringType,true)), Set()))
+
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
+
+      assert(!colCountMatch)
+      assert(missingFields.sameElements(Array(("emp_name", StringType))))
+      assert(extraFields.isEmpty)
+      assert(schemaMatch(actualBadRecords, df))
+      assert(dataMatch(actualBadRecords, df))
     }
 
-    it("should return false and extra columns when extra columns"){
+    it("should return the extra column and all records when extra column in all records"){
       import spark.implicits._
       val df = Seq((1, "Adam", 10), (2, "Eve", 12)).toDF("emp_id", "emp_name", "emp_age")
       writeData(df)
-      assert(SchemaValidation.validate(dataPath) ==
-        (false, Set(), Set(StructField("emp_age",IntegerType,true))))
+
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
+
+      assert(!colCountMatch)
+      assert(missingFields.isEmpty)
+      assert(extraFields.sameElements(Array(("emp_age", IntegerType))))
+      assert(schemaMatch(actualBadRecords, df))
+      assert(dataMatch(actualBadRecords, df))
     }
 
-    it("should return true and missing, extra columns when column emp_name mismatch"){
+    it("should return the missing column and all records when col name mismatch in all records, aka missing column"){
       import spark.implicits._
       val df = Seq((1, "Adam"), (2, "Eve")).toDF("emp_id", "emp_name_1")
       writeData(df)
-      assert(SchemaValidation.validate(dataPath) ==
-        (true,
-          Set(StructField("emp_name",StringType,true)),
-          Set(StructField("emp_name_1",StringType,true))))
+
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
+
+      assert(colCountMatch)
+      assert(missingFields.sameElements(Array(("emp_name", StringType))))
+      assert(extraFields.sameElements(Array(("emp_name_1", StringType))))
+      assert(schemaMatch(actualBadRecords, df))
+      assert(dataMatch(actualBadRecords, df))
     }
 
-    it("should return true and mismatch types when column type mismatch"){
+    it("should return the missing column and all records when col type mismatch in all records, aka missing column"){
       import spark.implicits._
       val df = Seq((1.0, "Adam"), (2.1, "Eve")).toDF("emp_id", "emp_name")
       writeData(df)
-      assert(SchemaValidation.validate(dataPath) ==
-        (true,
-          Set(StructField("emp_id",IntegerType,true)),
-          Set(StructField("emp_id",DoubleType,true))))
+
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
+
+      assert(colCountMatch)
+      assert(missingFields.sameElements(Array(("emp_id", IntegerType))))
+      assert(extraFields.sameElements(Array(("emp_id", DoubleType))))
+      assert(schemaMatch(actualBadRecords, df))
+      assert(dataMatch(actualBadRecords, df))
     }
 
-    it("should return true and no mismatch when null criteria match"){
+    it("should return NO Bad records when nullable columns are null"){
       import spark.implicits._
       val df = Seq((1, "Adam"), (2, null)).toDF("emp_id", "emp_name")
       writeData(df)
-      assert(SchemaValidation.validate(dataPath) ==  (true, Set(), Set()))
+
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
+
+      assert(colCountMatch)
+      assert(missingFields.isEmpty)
+      assert(extraFields.isEmpty)
+      assert(actualBadRecords.count() == 0)
+    }
+
+    it("should return Bad Records when NON-NULLABLE columns are null"){
+      import spark.implicits._
+      val df = Seq[(Integer, String)]((null, "Adam"), (2, null)).toDF("emp_id", "emp_name")
+      writeData(df)
+
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
+
+      assert(colCountMatch)
+      assert(missingFields.isEmpty)
+      assert(extraFields.isEmpty)
+
+      val expectedBadRecords =
+        Seq[(Integer, String)]((null, "Adam"))
+          .toDF("emp_id","emp_name")
+      assert(schemaMatch(actualBadRecords, expectedBadRecords))
+      assert(dataMatch(actualBadRecords, expectedBadRecords))
     }
   }
 
-  describe("Get Bad Records"){
-    it("should return true and bad records when null criteria mismatch"){
+  describe("Schema Validation, when records have multiple schema"){
+
+    it("should get bad records for missing fields for NON_NULLABLE columns in some rows") {
       import spark.implicits._
-      val df = Seq[(Integer, String)]((null, "Adam"), (2, "Eve")).toDF("emp_id", "emp_name")
+      val df = Seq((1, "Adam"), (2, "Eve")).toDF("emp_id", "emp_name")
       writeData(df)
+      val df1 = Seq(("Caty"), ("Dan")).toDF("emp_name")
+      writeData(df1, "append")
 
-      assert(SchemaValidation.validate(dataPath) ==  (true, Set(), Set()))
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
 
-      val expectedBadRecordsDF = Seq[(Integer, String)]((null, "Adam")).toDF("emp_id", "emp_name")
-      val actualBadRecords = SchemaValidation.getBadRecords(dataPath)
-      assert(actualBadRecords.schema.fields sameElements(expectedBadRecordsDF.schema.fields))
-      assert(actualBadRecords.collect() sameElements(expectedBadRecordsDF.collect()))
+      assert(colCountMatch)
+      assert(missingFields.isEmpty)
+      assert(extraFields.isEmpty)
+
+      val expectedBadRecords =
+        Seq[(Integer, String)]((null, "Caty"), (null, "Dan"))
+          .toDF("emp_id","emp_name")
+
+      assert(schemaMatch(actualBadRecords, expectedBadRecords))
+      // Since the ordering of columns within a row was not fixed
+      val expected = expectedBadRecords
+        .select(expectedBadRecords.col("emp_id"), expectedBadRecords.col("emp_name"))
+      val actual = actualBadRecords
+        .select(actualBadRecords.col("emp_id"), actualBadRecords.col("emp_name"))
+      assert(dataMatch(actual, expected))
+    }
+
+    it("should get NO bad records for missing fields for NULLABLE columns in some rows") {
+      import spark.implicits._
+      val df = Seq((1, "Adam"), (2, "Eve")).toDF("emp_id", "emp_name")
+      writeData(df)
+      val df1 = Seq((3), (4)).toDF("emp_id")
+      writeData(df1, "append")
+
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
+
+      assert(colCountMatch)
+      assert(missingFields.isEmpty)
+      assert(extraFields.isEmpty)
+      assert(actualBadRecords.count() == 0)
+    }
+
+    it("should get bad records for extra fields in some rows") {
+      import spark.implicits._
+      val df = Seq((1, "Adam"), (2, "Eve")).toDF("emp_id", "emp_name")
+      writeData(df)
+      val expectedBadRecords = Seq((1, "Adam", 10), (2, "Eve", 12))
+        .toDF("emp_id", "emp_name", "emp_age")
+      writeData(expectedBadRecords, "append")
+
+      val (colCountMatch, missingFields, extraFields, actualBadRecords) = SchemaValidation.validate(dataPath)
+
+      assert(!colCountMatch)
+      assert(missingFields.isEmpty)
+      assert(extraFields sameElements Array(("emp_age", IntegerType)))
+
+      assert(schemaMatch(actualBadRecords, expectedBadRecords))
+      assert(dataMatch(actualBadRecords, expectedBadRecords))
     }
   }
 }
