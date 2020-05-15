a13774c93b1efe8b6d09472fe0dc72155a32a4df
Nisha <nishasinha68@gmail.com>
2020-05-15 22:53:17 +0530

    Tests schema validation via Main method.

--- src/main/scala/HelloSpark.scala
+++ src/main/scala/HelloSpark.scala
@@ -7,14 +7,36 @@ object HelloSpark {
     val spark = SparkSession
       .builder
       .master("local[*]")
-      .appName("word count")
+      .appName("Hello Spark")
       .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
 
     import spark.implicits._
-
     val data = (1 to 50).toDS()
     data
       .filter(_ % 10 == 0)
       .foreach(println(_))
+
+    println("********* Running schema validation **************")
+    val dataPath = "data-to-test"
+
+    println("Given Schema")
+    SchemaValidation.givenSchema.foreach(println)
+
+    println("Input data read")
+    val dataDF = spark.read.option("mergeSchema", "true").load(dataPath)
+    dataDF.show
+
+    println("Running validation")
+    val result = SchemaValidation.validate(dataPath)
+
+    println("Schema validation result:", result._1)
+    println("Column count Match", result._2)
+    println("Missing Columns")
+    result._3.foreach(println)
+    println("Extra Columns")
+    result._4.foreach(println)
+    println("Bad Records")
+    result._5.show
   }
 }
